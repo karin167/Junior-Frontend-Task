3361dfac72cc39b3d010ef78960bde995a605ea2
karin yahud <karin167bz@gmail.com>
2021-08-22 22:56:04 +0200

    github-search-task-2

--- src/components/components.test.js
+++ src/components/components.test.js
@@ -4,6 +4,5 @@ import App from "./App";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Enter search term:/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument("./App");
 });
-
