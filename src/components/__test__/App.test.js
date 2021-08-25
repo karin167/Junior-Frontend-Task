658661629d0afdeff67d877f93e7f63d9b0e1d41
karin yahud <karin167bz@gmail.com>
2021-08-25 13:00:42 +0200

    adding react-teasig-library

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -2,29 +2,35 @@ import { render, screen } from "@testing-library/react";
 import App from "../../App";
 import "@testing-library/jest-dom";
 
+describe("testig-App.js", () => {
+  it("testin the title label from App.js", async () => {
+    render(<App title="label" />);
+    const headingElement = screen.getByTitle("label");
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  it("testing the the main div from App.js", async () => {
+    render(<App title="wrap-div" />);
+    const headingElement = screen.getByTestId("wrap-div");
+    expect(headingElement).toBeInTheDocument();
+  });
+});
+
+// getByText:
+
 // it("testin the text that come from the label from App.js", async () => {
 //   render(<App title="Enter" />);
 //   const headingElement = screen.getByText(/Enter/i);
 //   expect(headingElement).toBeInTheDocument();
 // });
 
+//getByRole:
+
 // it("testin the button from App.js", async () => {
 //   render(<App title="button" />);
 //   const headingElement = screen.getByRole("button");
 //   expect(headingElement).toBeInTheDocument();
 // });
-
-it("testin the title label from App.js", async () => {
-  render(<App title="label" />);
-  const headingElement = screen.getByTitle("label");
-  expect(headingElement).toBeInTheDocument();
-});
-
-it("testing the the main div from App.js", async () => {
-  render(<App title="wrap-div" />);
-  const headingElement = screen.getByTestId("wrap-div");
-  expect(headingElement).toBeInTheDocument();
-});
 
 //Find by text:
 
@@ -46,8 +52,8 @@ it("testing the the main div from App.js", async () => {
 
 // getAllByRole:
 
-it("getAllByRole", async () => {
-  render(<App title="button" />);
-  const headingElements = screen.getAllByRole("button");
-  expect(headingElements.length).toBe(1);
-});
+// it("getAllByRole", async () => {
+//   render(<App title="button" />);
+//   const headingElements = screen.getAllByRole("button");
+//   expect(headingElements.length).toBe(1);
+// });
